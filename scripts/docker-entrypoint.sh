2722ad7fa9f562bf4d0a490fc890a2abeb1be233
Aleksandr Soloshenko <capcom2me@gmail.com>
2022-08-17 00:40:04 +0700

    Fixed: some docker issues

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,4 +3,4 @@
 PORT=${PORT:-8000}
 
 python -m app db-init \
-    && gunicorn --bind=0.0.0.0:${PORT} --access-logfile=- --proxy-allow-from='*' -k uvicorn.workers.UvicornWorker server.main:app $@
+    && gunicorn --bind=0.0.0.0:${PORT} --access-logfile=- --proxy-allow-from='*' -k uvicorn.workers.UvicornWorker app.server:app $@
